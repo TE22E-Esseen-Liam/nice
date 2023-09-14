17714073e1ea80415d0599134a5900d46026c6ec
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2023-09-14 10:47:58 +0200

    lektion

---
+++ övning/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        sum = sum + 5;
+        sum += 5;
+        sum++;
+
+        sum *= 2;
+
+        Console.WriteLine($"Summan är {sum}!");
+
+        Console.ReadLine();
+    }
+}
